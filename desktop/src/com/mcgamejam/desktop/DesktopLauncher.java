24045f7a6cd69d0878ceab6c0e977f21ba0f05b4
Kelvin Jin <kkj9@cornell.edu>
2015-03-28 01:48:52 -0400

    adding libgdx game

---
+++ desktop/src/com/mcgamejam/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mcgamejam.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mcgamejam.McGameJam;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new McGameJam(), config);
+	}
+}
